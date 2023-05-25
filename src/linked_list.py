07e0afe904d6a1c01e368b0a7fac809f92cdfae4
Vladislav_Poroshin <vld.poroshin@gmail.com>
2023-05-25 15:25:27 +0400

    Created a new class LinkedList and tests for it.

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,37 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        if self.head is None:
+            node = Node(data)
+        else:
+            node = Node(data, self.head)
+        self.head = node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data)
+        cur_node = self.head
+        if cur_node is None:
+            self.head = self.tail = new_node
+            return
+        while cur_node.next_node is not None:
+            cur_node = cur_node.next_node
+        cur_node.next_node = new_node
+        self.tail = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,14 @@
-"""Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+from src.linked_list import LinkedList
+import unittest
+
+class TestStack(unittest.TestCase):
+
+    def test_linked_list(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+
+        self.assertEqual(str(ll), "{'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None")
+
